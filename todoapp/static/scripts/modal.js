037088744693f87343f85a55767df18813acc560
ulysses316 <ulises316@live.com.mx>
2022-09-11 03:04:31 -0500

    CRUD Complete

--- todoapp/static/scripts/modal.js
+++ todoapp/static/scripts/modal.js
@@ -23,11 +23,21 @@ const closeModalUpdate = () => {
 }
 
 const openModalUpdate = (e) =>{
-    let modal = document.querySelector('.modal-update');
-    modal.style.display = "flex";
+    // Note info
     let cardID = e.path[2].getAttribute("cardNumber");
+    let cardTitle = e.path[2].childNodes[1].childNodes[1].innerText;
+    let cardBody = e.path[2].childNodes[1].childNodes[3].innerText;
+    // Modal info
+    let modal = document.querySelector('.modal-update');
     let formAction = modal.childNodes[1].childNodes[3];
-    formAction.action = `${cardID}/update`
+    let formTitle = modal.childNodes[1].childNodes[3].childNodes[3];
+    let formBody = modal.childNodes[1].childNodes[3].childNodes[7];
+    
+    formAction.action = `${cardID}/update`;
+    formTitle.value = cardTitle;
+    formBody.innerText = cardBody;
+    
+    modal.style.display = "flex";
 }
 
 let btnopenModalUpdate = document.querySelectorAll(".open-modal-update");
